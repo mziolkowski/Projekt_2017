ad201e1aed918e394a830e18c87748088dfcc7be
M.Ziolkowski <ziolek0321@gmail.com>
2017-12-20 19:26:12 +0100

    init

---
+++ src/sample/Communicator.java
@@ -0,0 +1,150 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import jssc.SerialPort;
+import static jssc.SerialPort.MASK_RXCHAR;
+import jssc.SerialPortEvent;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+public class Communicator extends Application {
+
+    SerialPort stmPort = null;
+    ObservableList<String> portList;
+
+    Label labelValue;
+
+    private void detectPort() {
+
+        portList = FXCollections.observableArrayList();
+
+        String[] serialPortNames = SerialPortList.getPortNames();
+        for (String name : serialPortNames) {
+            System.out.println(name);
+            portList.add(name);
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        labelValue = new Label();
+
+        detectPort();
+        final ComboBox comboBoxPorts = new ComboBox(portList);
+        comboBoxPorts.valueProperty()
+                .addListener(new ChangeListener<String>() {
+
+                    @Override
+                    public void changed(ObservableValue<? extends String> observable,
+                                        String oldValue, String newValue) {
+
+                        System.out.println(newValue);
+                        disconnectSTM32();
+                        connectSTM32(newValue);
+                    }
+
+                });
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(
+                comboBoxPorts, labelValue);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public boolean connectSTM32(String port) {
+
+        System.out.println("connect STM32");
+
+        boolean success = false;
+        SerialPort serialPort = new SerialPort(port);
+        try {
+            serialPort.openPort();
+            serialPort.setParams(
+                    SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            serialPort.setEventsMask(MASK_RXCHAR);
+            serialPort.addEventListener((SerialPortEvent serialPortEvent) -> {
+                if (serialPortEvent.isRXCHAR()) {
+                    try {
+                        String st = serialPort.readString(serialPortEvent
+                                .getEventValue());
+                        System.out.println(st);
+
+                        //Update label in ui thread
+                        Platform.runLater(() -> {
+                            labelValue.setText(st);
+                        });
+
+                    } catch (SerialPortException ex) {
+                        Logger.getLogger(Communicator.class.getName())
+                                .log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            });
+
+            stmPort = serialPort;
+            success = true;
+        } catch (SerialPortException ex) {
+            Logger.getLogger(Communicator.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            System.out.println("SerialPortException: " + ex.toString());
+        }
+
+        return success;
+    }
+
+    public void disconnectSTM32() {
+
+        System.out.println("disconnect STM32()");
+        if (stmPort != null) {
+            try {
+                stmPort.removeEventListener();
+
+                if (stmPort.isOpened()) {
+                    stmPort.closePort();
+                }
+
+            } catch (SerialPortException ex) {
+                Logger.getLogger(Communicator.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        disconnectSTM32();
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
